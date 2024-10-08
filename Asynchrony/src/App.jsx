5df07ada40761167caec9cb90737c325a9171cb4
Thabang Selowa <mohale20cobra@gmail.com>
2024-10-08 16:16:22 +0200

    Add states function

---
+++ Asynchrony/src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  // State to hold posts and error messages
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true); // To show loading state
+
+  }
+
+export default App;
